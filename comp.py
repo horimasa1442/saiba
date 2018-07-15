549a819878063a502453d44ec24239110bc79bd1
chiwa1442 <horimasa1442@gmail.com>
2018-07-15 14:12:43 +0900

    ErrorMesh is implemented. ToDo : test

--- comp.py
+++ comp.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 from open3d import *
 from statistics import *
+from MeshCompFunc import *
 
 def calculateDiffs(pcTarget, pcSource, sourceTree, thre):
     mind = 1000
@@ -10,6 +11,7 @@ def calculateDiffs(pcTarget, pcSource, sourceTree, thre):
     sumd2 = 0.0
     nCorrect = 0
     nTotal = len(np.asarray(pcTarget.points))
+    errors = [0.0] * nTotal
     for j in range(nTotal):
         [k, idx, _] = sourceTree.search_knn_vector_3d(pcTarget.points[j], 1)
         p0 = np.asarray(pcTarget.points)[j, :]
@@ -18,6 +20,7 @@ def calculateDiffs(pcTarget, pcSource, sourceTree, thre):
         d = np.linalg.norm(p0 - p1)
         sumd += d
         sumd2 += d*d
+        errors[j] = d
 
         if mind > d:
             mind = d
@@ -37,7 +40,50 @@ def calculateDiffs(pcTarget, pcSource, sourceTree, thre):
     stas.numTotal = nTotal
     stas.threshold = thre
 
-    return stas
+    return stas, errors
+
+def CalcColorMap(v, vmin, vmax):
+    
+    rgb_val = [255.0, 255.0, 255.0]
+    
+    if (v < vmin):
+         v = vmin
+    if (v > vmax):
+         v = vmax
+    dv = vmax - vmin
+    
+    if (v < (vmin + 0.25 * dv)):
+        rgb_val[0] = 0
+        rgb_val[1] = (4.0 * (v - vmin) / dv) * 255.0
+    
+    elif (v < (vmin + 0.5 * dv)):
+        rgb_val[0] = 0
+        rgb_val[2] = (1.0 + 4.0 * (vmin + 0.25 * dv - v) / dv) * 255.0
+
+    elif (v < (vmin + 0.75 * dv)):
+        rgb_val[0] = (4.0 * (v - vmin - 0.5 * dv) / dv) * 255.0
+        rgb_val[2] = 0
+
+    else:
+        rgb_val[1] = (1.0 + 4.0 * (vmin + 0.75 * dv - v) / dv) * 255.0
+        rgb_val[2] = 0
+    
+    return map(int,rgb_val)
+
+
+def rewriteColorForErrorMap(pc, errors, max):
+    nTotal = len(np.asarray(pc.points))
+    if nTotal != len(errors):
+        raise ValueError("errors and points are not equal")
+        return
+
+    for i in range(nTotal):
+        rgb = CalcColorMap(errors[i], 0, max)
+        np.asarray(pc.colors)[i, :] = [rgb[0], rgb[1], rgb[2]]
+
+    return
+
+
 
 if __name__ == "__main__":
     
@@ -51,14 +97,32 @@ if __name__ == "__main__":
 
     pcdg = read_point_cloud(GtPath)
     GtTree = KDTreeFlann(pcdg)
+    errors = []
+    stass = []
+    pcs = []
 
     for i in range(len(sPaths)):
         print(f"Source Mesh : {sPaths[i]}")
-        pc = read_point_cloud(sPaths[i])
+        tempPC = read_point_cloud(sPaths[i])
+        pcs.append(tempPC)
         print("precision")
-        print(calculateDiffs(pc, pcdg, GtTree, thre))
+        stasTemp, errorTemp = calculateDiffs(pcs[i], pcdg, GtTree, thre)
+        stass.append(stasTemp)
+        errors.append(errorTemp)
+        print(stass[i])
 
 
-        sTree = KDTreeFlann(pc)        
+        sTree = KDTreeFlann(pcs[i])        
         print("completness")
-        print(calculateDiffs(pcdg, pc, sTree, thre))
+        stasTemp, _ = calculateDiffs(pcdg, pcs[i], sTree, thre)
+        print(stasTemp)
+
+    maxError = 0.0
+
+    for i in len(stass):
+        if stass[i].max > maxError:
+            maxError = stass[i].max
+
+    for i in len(errors):
+        rewriteColorForErrorMap(pcs[i], errors[i], maxError)
+        write_point_cloud("output" + str(i) + ".ply", pcs[i])
